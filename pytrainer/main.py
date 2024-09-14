2890c179a44b04e8a87e6f17b949d790b26e32ad
handbob <handbob@icloud.com>
2024-09-14 16:50:25 +0200

    update

--- pytrainer/main.py
+++ pytrainer/main.py
@@ -1,27 +1,11 @@
-from scripts.classes.house import House
-from scripts.classes.car import Car
-from scripts.classes.person import Person
-
 from sys import argv
 
-print('Hello from Python!\n')
-
-house = House("1234 Elm St", 4, 250000.0, "Single Family", 1995)
-house.display_info()
-
-person = Person("John Doe", 30, "Male", "456 Elm Street", "555-1234")
-person.display_info()
-
-car = Car("Toyota", "Camry", 2020, 20000.0, "Blue", 15000)
-car.display_info()
-
 VERSION = '1.0.0'
-
 
 def print_help():
     help_message = '''usage: pytrainer [options] <command> 
 
-example: pytrainer -t -m tictactoe
+example: pytrainer -t -m tictactoe | cards | chess | hangman
 
 options:
     -v, --version         show version
